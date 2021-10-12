1f1921c8deaf5f820fb853ec38ec6c452f5fbf7b
sarthaksshetty <sarthaksshetty@gmail.com>
2021-10-12 12:06:52 +0530

    Initial commmits

--- django_form/django_form/settings.py
+++ django_form/django_form/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p3h)gl)!6uu&bs+(_h_=)^w!-*a(#$78@wr=kwp6amznmtpnb8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
