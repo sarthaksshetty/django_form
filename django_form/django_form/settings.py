951f05139eb4a8619986195ddad424de7287aa25
sarthaksshetty <sarthaksshetty@gmail.com>
2021-10-12 12:21:50 +0530

    added static

--- django_form/django_form/settings.py
+++ django_form/django_form/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 LOGIN_REDIRECT_URL = 'home'
